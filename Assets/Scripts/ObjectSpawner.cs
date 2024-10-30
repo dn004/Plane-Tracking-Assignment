9fb7e5584a676315478f4b7ea8d1d486c896ee41
dn004 <d.ntwari@alustudent.com>
2024-10-30 11:49:46 +0200

    update

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -6,42 +6,43 @@ using System.Collections.Generic;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject objectPrefab;
-    private GameObject createdObject;
-    private bool objectSpawned = false;
-    public ColorSwitcher colorSwitcher;
-    private ARRaycastManager arRaycastManager;
-    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    public GameObject prefabToSpawn;
+    private GameObject placedObject;
+    private bool hasSpawnedObject = false;
+    public ColorChanger colorChanger;
+    private ARRaycastManager raycastManager;
+    private List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
 
-    private void Start()
+    void Awake()
     {
-        arRaycastManager = GetComponent<ARRaycastManager>();
+        raycastManager = GetComponent<ARRaycastManager>();
     }
 
-    private void Update()
+    void Update()
     {
-        Vector2 inputPosition = Vector2.zero;
+        Vector2 screenPosition = GetTouchOrClickPosition();
+        if (screenPosition == Vector2.zero || hasSpawnedObject)
+            return;
 
+        if (raycastManager.Raycast(screenPosition, raycastHits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose placementPose = raycastHits[0].pose;
+            placedObject = Instantiate(prefabToSpawn, placementPose.position, placementPose.rotation);
+            hasSpawnedObject = true;
+            colorChanger.SetTargetObject(placedObject);
+        }
+    }
+
+    private Vector2 GetTouchOrClickPosition()
+    {
         if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
         {
-            inputPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            return Touchscreen.current.primaryTouch.position.ReadValue();
         }
         else if (Mouse.current != null && Mouse.current.leftButton.isPressed)
         {
-            inputPosition = Mouse.current.position.ReadValue();
+            return Mouse.current.position.ReadValue();
         }
-        else
-        {
-            return;
-        }
-
-
-        if (!objectSpawned && arRaycastManager.Raycast(inputPosition, hits, TrackableType.PlaneWithinPolygon))
-        {
-            Pose hitPose = hits[0].pose;
-            createdObject = Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
-            objectSpawned = true;
-            colorSwitcher.AssignObject(createdObject);
-        }
+        return Vector2.zero;
     }
 }
